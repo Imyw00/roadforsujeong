b247592148008e2b8fd40df433eef06b2051c37b
Your Name <suncastle023>
2023-07-17 16:32:38 +0900

    지도 기능추가

--- app/src/main/java/com/example/Start_Screen/TodayRecom.java
+++ app/src/main/java/com/example/Start_Screen/TodayRecom.java
@@ -2,15 +2,14 @@ package com.example.Start_Screen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.Start_Screen.R;
-
 import android.os.Bundle;
 
-public class certification extends AppCompatActivity {
+
+public class TodayRecom extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_certification);
+        setContentView(R.layout.activity_todayrecom);
     }
 }
--- app/src/main/java/com/example/Start_Screen/certification.java
+++ app/src/main/java/com/example/Start_Screen/certification.java
@@ -4,13 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.Start_Screen.R;
-
-public class TodayRecom extends AppCompatActivity {
+public class certification extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_todayrecom);
+        setContentView(R.layout.activity_certification);
     }
 }
